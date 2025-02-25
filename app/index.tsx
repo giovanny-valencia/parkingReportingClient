3ee39cfc347c4b65df3757cb6de552023f2a0eb1
giovanny-valencia <156955848+giovanny-valencia@users.noreply.github.com>
2025-02-25 02:22:46 -0500

    removed default files

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Why's it say index on the top?</Text>
+    </View>
+  );
+}
